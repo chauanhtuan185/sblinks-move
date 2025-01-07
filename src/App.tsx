ad30081f8a12ce9b067f47acb93496b39705f876
Kay ( Andrew ) <chauanhtuan185@gmail.com>
2025-01-07 18:06:28 +0700

    int

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,19 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import { Route, Routes } from 'react-router-dom';
+import { Route, Routes , BrowserRouter } from 'react-router-dom';
 import Home from './pages/Home';
 import Sui from './pages/Sui';
 import Aptos from './pages/Aptos';
 
 function App() {
   return (
+    <BrowserRouter>
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/swap" element={<Home />} />
       <Route path="/sui" element={<Sui />} />
       <Route path="/aptos" element={<Aptos />} />
     </Routes>
+    </BrowserRouter>
   );
 }
 
